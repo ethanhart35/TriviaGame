27d98c454203035883ea528c182d91f052c936f1
Ethan Hart <ethanhart35@gmail.com>
2020-01-11 23:34:23 -0700

    implemented functional countdown timer

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,6 +2,25 @@ $( document ).ready(function() {
     console.log( "ready!" );
 
 
+//global vars
+var correct = 0;
+var incorrect = 0;
+var time = 60;
+
+
+//function to count down from 60s
+
+setInterval(function() {
+    time--
+    $("#timer").html(time)
+    console.log(time)
+}, 1000);
+
+//check if answers are correct when time runs out or button is clicked
+
+
+
+//when timer gets to 0 or button is clicked then timer stops and score is shown.
 
 
 
@@ -29,10 +48,4 @@ $( document ).ready(function() {
 
 
 
-
-
-
-
-
-    
 });
